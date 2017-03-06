fbac602a55f9c757e5d15aa62e095cbb207eb19d
mayownrhyme <lxyownrhyme@gmail.com>
2017-03-06 22:36:15 +0800

    3.6

--- oppo-aodv.cc
+++ oppo-aodv.cc
@@ -1,24 +1,100 @@
+#include "oppo-aodv.h"
 #include <string.h>
 #include <omnetpp.h>
-
+#include<algorithm>
 #define UPDATETIME 5.0
 #define SENDDATA 100
-using namespace omnetpp;
 
+Define_Module(Node);
 Node::Node(){
 
-};
-Node::~Node(){};
+}
+Node::~Node(){
+
+}
 
 void Node::initialize(){
-    cMessage *update = new cMessage("update");
-    cMessage *rreq = new cMessage("rreq");
-    cMessage *rrep = new cMessage("rrep");
-    cMessage *senddata = new cMessage("senddata");
-    schduleAt(simTime()+UPDATETIME,senddata);
-    schduleAt(simTime()+SENDDATA,update);
+    update = new cMessage("update");
+    senddata = new cMessage("senddata");
+    //---test code---
+//    if (getIndex()==0){
+//        scheduleAt(0.0,update);
+//    }
+    //--------
+      scheduleAt(simTime()+uniform(0,UPDATETIME),update);
+//    scheduleAt(simTime()+SENDDATA,senddata);
 }
 
 void Node::handleMessage(cMessage *msg){
+    if(msg->isSelfMessage()){
+        if(msg == update){
+               handleUPDATEDATA();//send update packet
 
+           }
+    }else{
+        /*
+         * send update ack
+         * */
+        if(mycast<UPDATEDATA*>(msg)!=NULL ){
+            //return routetable
+            vector<unsigned int> v;
+            for(deque<Neighbor>::iterator i =neighbortable.begin();i!=neighbortable.end();i++)
+            {
+                  unsigned int temp=(*i).nodeid;
+                  v.push_back(temp);
+            }
+            NeighborSet sendtemp(v);
+            UPDATEACK *t = new UPDATEACK("updateack");
+            t->setNeighborset(sendtemp);
+            send(t,"g$o",(msg->getArrivalGateId() -  msg->getArrivalGate()->getBaseId()));
+            EV<<"go to next";
+            update = new cMessage("update");
+            scheduleAt(simTime()+uniform(0,UPDATETIME),update);
+        }
+        if(mycast<UPDATEACK*>(msg)!=NULL){
+            UPDATEACK *t=check_and_cast<UPDATEACK*>(msg);
+            deque<Neighbor>::iterator i;
+            for(i =neighbortable.begin();i!=neighbortable.end();i++)
+            {
+
+                if((*i).nodeid == (t->getSenderModule()->getIndex())){
+                    //calculate delay and successful rate;
+                    (*i).neighborvector=(t->getNeighborset()).neighborvector;
+                }
+            }
+
+            if(i == neighbortable.end())
+            {
+                Neighbor neighbor;
+                neighbor.nodeid= t->getSenderModule()->getIndex();
+                neighbor.neighborvector=(t->getNeighborset()).neighborvector;
+                (neighbor.delay).push_back(0);
+                (neighbor.SuccessfulRate).push_back(1);
+                neighbortable.push_back(neighbor);
+            }
+            if(neighbortable.size()>10)
+                neighbortable.pop_front();
+
+            /*
+             * test code
+             * */
+//            for(auto j: neighbortable){
+//                EV<<j.nodeid<<endl;
+//            }
+            delete t;
+        }
+    }
+}
+
+void Node::handleUPDATEDATA(){
+    UPDATEDATA *updatedata =new UPDATEDATA("updatedata");
+    broadcastUPDATE(updatedata);
+}
+
+void Node::broadcastUPDATE(UPDATEDATA *msg){
+    int n = gateSize("g");
+    for(int i = 0 ;i<n ;i++){
+        UPDATEDATA *copy=msg->dup();
+        send(copy,"g$o",i);
+    }
 }
--- oppo-aodv.h
+++ oppo-aodv.h
@@ -1,38 +1,38 @@
+#ifndef __OPPO_AODV_
+#define __OPPO_AODV_
 #include <string.h>
 #include <omnetpp.h>
 #include <vector>
+#include <deque>
+#include "MyMessage_m.h"
 
 using namespace omnetpp;
+using namespace std;
+
 
 class Node : public cSimpleModule{
 private:
-    vector<Neighbor> neighbor;
+    deque<Neighbor> neighbortable;
     vector<RestorePath> resPath;
+    cMessage *update ;
+    cMessage *rreq ;
+    cMessage *senddata ;
 
 protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-
+    //virtual void finish() override;
+    void handleRREP(AODVRREP *msg);
+    void handleRREQ(AODVRREQ *msg);
+    void handleDATAACK(AODVDATAACK *msg);
+    void handleDATA(AODVDATA *msg);
+    void handleUPDATEDATA();
+    void handleUPDATEACK(UPDATEACK *msg);
+    void broadcastUPDATE(UPDATEDATA *msg);
 public:
     Node();
     virtual ~Node();
 };
 
-
-
-struct Neighbor{
-    unsigned int Nodeid;
-    double SuccessfulRate;
-    double delay;
-    vector<unsigned int> neighborSet;
-};
-
-struct RestorePath{
-    unsigned int Nodeid;
-    vector<unsigned int> fowSet;
-    //out of date time;
-    simtime_t outtime;
-};
-
-
+#endif
 
